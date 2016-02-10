32c9f88fafde4ba0b618cf804b6e2e9f3116a5b3
Richard White <richard.white@kurtgeiger.com>
2016-02-10 12:52:39 +0000

    Completed lesson 12.

---
+++ lesson12.js
@@ -0,0 +1,25 @@
+Vue.component('alert', {
+    template: '#alert-template',
+    props: ['type'],
+    data: function() {
+        return {
+            show: true
+        };
+    },
+
+    computed: {
+        alertClasses: function() {
+            var type = this.type;
+
+            return {
+                'Alert' : true,
+                'Alert--Success' : type == 'success',
+                'Alert--Error' : type == 'error'
+            }
+        }
+    }
+});
+
+new Vue({
+    el: 'body'
+});
---
+++ main.js
@@ -0,0 +1,7 @@
+Vue.component('alert', {
+    template: '#alert-template'
+});
+
+new Vue({
+    el: 'body'
+});
