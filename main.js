78a9de8fc52cef5fd717eb462d27c961b3510431
Richard White <richard@rabbitsfoot.co.uk>
2016-02-10 21:08:01 +0000

    Completed lesson 9.

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-Vue.component('alert', {
-    template: '#alert-template'
+Vue.component('tasks', {
+    props: ['list'],
+
+    template: '#tasks-template',
+
+    computed: {
+        remaining: function() {
+            return this.list.filter(this.inProgress).length;
+        }
+    },
+
+    methods: {
+        isCompleted: function(task) {
+            return task.completed;
+        },
+        inProgress: function(task) {
+            return ! this.isCompleted(task);
+        },
+        deleteTask: function(task) {
+            this.list.$remove(task);
+        },
+        clearCompleted: function() {
+            this.list = this.list.filter(this.inProgress);
+        }
+    }
 });
 
 new Vue({
-    el: 'body'
+    el: '#app',
+
+    data: {
+        tasks: [
+            { body:'Go to the store', completed: false },
+            { body:'Go to the bank', completed: false },
+            { body:'Go to the doctor', completed: true }
+        ]
+    },
+    methods: {
+        toggleCompletedFor: function(task) {
+            task.completed = ! task.completed;
+        }
+    }
 });
